bd1295b2b76d99ae4d6c74459fa2e40b09ec675b
AleNimo <aledn2000@gmail.com>
2022-10-13 23:40:52 -0300

    TPLAB2

---
+++ workspace/ADCMSIS/inc/filter.h
@@ -0,0 +1,17 @@
+/*
+===============================================================================
+ Name        : filter.h
+ Author      : Israel Pavelek, Cesar Fuoco
+ Version     : 1.0
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#ifndef LOWPASS_H_
+#define LOWPASS_H_
+
+
+
+
+#endif /* LOWPASS_H_ */
